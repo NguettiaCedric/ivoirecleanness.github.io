ef1a12f8015d48c9b56cbff749c095a38f75f2ec
NguettiaCedric <88029325+NguettiaCedric@users.noreply.github.com>
2022-12-29 17:47:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+(function() {
+    "use strict";
+  
+    const select = (el, all = false) => {
+      el = el.trim()
+      if (all) {
+        return [...document.querySelectorAll(el)]
+      } else {
+        return document.querySelector(el)
+      }
+    }
+  
+    /**
+     * Preloader
+     */
+    let preloader = select('#preloader'); 
+    if (preloader) {
+      window.addEventListener('load', () => {
+        preloader.remove()
+      });
+    }
+  })()
